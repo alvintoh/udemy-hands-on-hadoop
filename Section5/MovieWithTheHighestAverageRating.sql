f52f8977bd14b11aae678d7e0bf0fb2a8e52b3eb
Alvin Toh <alvin4292@gmail.com>
2018-02-02 18:48:43 +0800

    Correct SQL code
    put filter condition > 10 at the end for select statement 2

--- Section5/MovieWithTheHighestAverageRating.sql
+++ Section5/MovieWithTheHighestAverageRating.sql
@@ -1,9 +1,9 @@
 CREATE VIEW IF NOT EXISTS avgRatings AS
-SELECT movieID, avg(rating) as averageRating, count(movieID) as ratingCount
+SELECT movieID, AVG(rating) as averageRating, COUNT(movieID) as ratingCount
 FROM ratings
-WHERE ratingCount > 10
 GROUP BY movieID
 ORDER BY averageRating DESC;
 
-SELECT n.title, t.averageRating, t.ratingCount 
-FROM avgRatings t JOIN names n ON t.movieID = n.movieID;
+SELECT n.title, averageRating, ratingCount 
+FROM avgRatings t JOIN names n ON t.movieID = n.movieID
+WHERE t.ratingCount > 10;
